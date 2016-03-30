c8b263a54e608963b67406acb05b833d50566f63
Sal Espinosa <sespinos@gmail.com>
2016-03-30 06:00:45 -0600

    Add Database Cleaner require and fixer for DC error

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,7 @@ Bundler.require
 require File.expand_path("../../config/environment", __FILE__)
 require 'minitest/autorun'
 require 'capybara'
+require 'database_cleaner'
+DatabaseCleaner.strategy = :truncation, {except: %w[public.schema.migrations]}
 
 Capybara.app = FilmFile
